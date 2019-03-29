5b65f4ff388efd2d067fb14e483874e512048cd7
Teguh <teguh.h87@gmail.com>
2019-03-29 17:04:41 +0700

    update user model

--- mixins/base.py
+++ mixins/base.py
@@ -1,10 +1,8 @@
 import uuid
-from sqlalchemy import Column, String
+from sqlalchemy import Column, String, Integer
 
-def generate_uuid():
-    return str(uuid.uuid4())
 
 class BaseMixin(object):
     __abstract__ = True
 
-    id = Column(String, name="id", primary_key=True, default=generate_uuid)
+    id = Column(Integer, name="id", primary_key=True)
