dd489bd7189e32e46ddbe11033bc24554a8e319b
Teguh <teguh.h87@gmail.com>
2019-03-29 16:54:38 +0700

    update user model

--- model/user.py
+++ model/user.py
@@ -1,13 +1,14 @@
-from sqlalchemy import Column, String, Boolean
+from sqlalchemy import Column, String, Integer, Boolean
 from model.base import Model
 from mixins.timestamp import TimestampMixin
 
-class User(Model, TimestampMixin):
+class User(Model):
 
     __abstract__ = True
 
     __tablename__ = 'user'
 
+    id          = Column(Integer, name="id", primary_key=True)
     email       = Column(String(75))
     first_name  = Column(String(30))
     last_name   = Column(String(30))
