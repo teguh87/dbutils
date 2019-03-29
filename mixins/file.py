815832f57b530b3dc7f071d6db46c66b37af161e
Teguh <teguh.h87@gmail.com>
2019-03-29 11:19:21 +0700

    add user model

--- mixins/file.py
+++ mixins/file.py
@@ -5,3 +5,7 @@ class FileMixins(TimestampMixin):
     filename = Column(String(120), name="filename", nullable=False)
     path = Column(String(255), name="path", nullable=False)
     size = Column(Integer(), name="size")
+
+
+    def __repr__((self):
+        pass
---
+++ model/user.py
@@ -0,0 +1,13 @@
+from sqlalchemy import Column, String, Boolean
+from model.base import Model
+from mixins.timestamp import TimestampMixin
+
+model = Model()
+
+class User(model, TimestampMixin):
+    email       = Column(String(75), 'email')
+    first_name  = Column(String(30), 'first_name')
+    last_name   = Column(String(30), 'last_name')
+    is_active   = Column(Boolean, unique=False, default=True)
+    username    = Column(String(30), unique=True, nullable=False),
+    password    = Column(String(128), nullable=False)
